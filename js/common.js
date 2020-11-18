37eaa942c236c03f473cc2f71583bbbcea6576d3
som7337 <73566244+som7337@users.noreply.github.com>
2020-11-18 15:55:27 +0900

    update

---
+++ js/common.js
@@ -0,0 +1,19 @@
+$(function(){
+  // var pLeng = $(".section_maint_slide p").length;
+  // // console.log(pLeng);
+  // var secMain = $(".section_maint_slide")
+  // var pSub = $("<p>Visulal Portfolio</p>");
+  //
+  // $(".section_maint_slide").mouseleave(function(){
+  //   $(this).animate({ transform : translateX(-100 + "%") },"slow","swing",function(){
+  //     $(".section_maint_slide p:first").append(pSub);
+  //   };
+  // });
+  //
+  // var timerID = setInterval(function(){
+  //   secMain.mouseleave();
+  // },3000);
+
+
+
+});// document ready
